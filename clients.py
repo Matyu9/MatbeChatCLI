9482ce60139c2f63472ea13f69e92cc0f64b2ef9
mathieu <mathieu@degueurce.tk>
2022-03-26 11:50:42 +0100

    First Commit

---
+++ clients.py
@@ -0,0 +1,61 @@
+# let's create the client part of a chat with socket programming
+
+# we will use sockets to connect to the server
+# we will use the same server as in the previous exercise
+# we will use the same port and ip as in the previous exercise
+# we will use the same protocol as in the previous exercise
+
+# we will use threads for the client because I want to send and receive messages at the same time
+
+import socket
+import threading
+import hashlib
+
+# create the class for the client thread
+class ClientThread(threading.Thread):
+    def __init__(self, client_socket, client_address):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.client_address = client_address
+
+    def run(self):
+        # while loop to receive messages from the server
+        while True:
+            # receive the message from the server
+            message = self.client_socket.recv(1024).decode()
+            # if the message is empty, the client has disconnected
+            if message == '':
+                break
+
+            # print the message
+            print(message)
+
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# create a tuple with the ip and port
+server_address = ('127.0.0.1', 1217)
+
+
+# create a try-except block to catch errors
+try:
+    # connect to the server
+    s.connect(server_address)
+
+    # create a thread for the client
+    client_thread = ClientThread(s, server_address)
+    client_thread.start()
+
+    # while loop to send messages to the server
+    while True:
+        # get the message from the user
+        message = input(">> ")
+        # send the message to the server
+        s.send(message.encode())
+
+except Exception as e:
+    # if an error occurs, print the error
+    print("An error has occurred: {}".format(e))
+    # close the socket
+    s.close()
---
+++ server.py
@@ -0,0 +1,117 @@
+# let's create the server part of a chat with socket programming
+# we will use the socket library
+# we will use the threading library
+# we will use the time library
+# we will use the random library
+# we will use the sqlite3 library for storing the messages
+# we will use hashlib library for the chat encryption in SHA256
+
+import socket
+import threading
+import time
+import random
+import sqlite3
+import hashlib
+
+# create a socket object
+# AF_INET is the address family
+# SOCK_STREAM is the socket type
+# we will use TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# we will create a database, is named chat.db
+# we will use the sqlite3 library
+conn = sqlite3.connect('chat.db', check_same_thread=False)
+# we will create a cursor
+c = conn.cursor()
+
+# we will create a table named log_chat
+# we will use the sqlite3 library
+c.execute('''CREATE TABLE IF NOT EXISTS log_chat (timestamp text, username text, message text)''')
+
+user = []  # list of users
+
+
+# create a function 'broadcast' to send messages to all connected clients
+def broadcast(message):
+    # iterate over the list of clients
+    for client in user:
+        # if client is the sender, skip it
+        if client != s:
+            # send the message to the client
+            client.send(message)
+        else:
+            continue
+
+# we will create a class for clients that will use threads
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print("New connection added: ", clientAddress)
+        # add the client adress to the list of clients
+        user.append(self.csocket)
+
+    # function for the thread
+    def run(self):
+        # create a random name for the client
+        name = random.randrange(0, 1000)
+        # send the name to the client
+        self.csocket.send(str(name).encode())
+
+        # loop forever who will receive messages and broadcast them
+        while True:
+            # receive the message
+            message = self.csocket.recv(2048)
+            # if the message is empty, close the connection
+            if not message:
+                end_message = "Client %s is offline" % str(name)
+                print(end_message)
+                broadcast(end_message.encode())
+                # remove the client from the list of clients
+                user.remove(self.csocket)
+                # close the connection with the client
+                self.csocket.close()
+                break
+            # if the message is not empty, broadcast it
+            else:
+                # if message starts 'quit', close the connection and broadcast end message
+                if message.decode() == 'quit':
+                    end_message = "Client %s is offline" % str(name)
+                    print(end_message)
+                    broadcast(end_message.encode())
+                    # remove the client from the list of clients
+                    user.remove(self.csocket)
+                    # close the connection with the client
+                    ClientThread.csocket.close()
+                    break
+                # insert the message to the database
+                c.execute("INSERT INTO log_chat VALUES (?,?,?)", (time.strftime("%d/%m/%Y"), str(name), message.decode()))
+                # commit the changes
+                conn.commit()
+                # send the message to all clients
+                broadcast(message)
+                # print the message
+                print(message.decode())
+
+# we will use the IP address of the server
+# we will use the port number
+# we will bind the socket to the address
+
+(host, port) = ("127.0.0.1", 1217)
+s.bind((host, port))
+
+# we will listen for 5 connections
+s.listen(5)
+
+# print 'Server Started on ' + host + ':' + str(port)
+print('Server Started on ' + host + ':' + str(port))
+
+# we will create a loop to accept connections
+while True:
+    # accept connections
+    csocket, cAddress = s.accept()
+    # create a new thread for the client
+    # we will pass the client address and the client socket
+    # we will start the thread
+    ClientThread(cAddress, csocket).start()
